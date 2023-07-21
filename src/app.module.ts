636af57356b676f9dce38d99b41ae6487a16b67a
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-21 14:53:17 -0300

    feat: added db info for connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: "pedrohsj.dev",
+      password: "14863421amw",
+      database: "vercode_db",
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
