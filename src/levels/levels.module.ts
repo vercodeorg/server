187722c40d3b483ab22e6e9ea572ef23e2ac8a62
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-28 13:38:49 -0300

    feat: add User entity to levels module

--- src/levels/levels.module.ts
+++ src/levels/levels.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Level } from 'src/entities/level.entity';
+import { User } from 'src/entities/user.entity';
 import { LevelsController } from './levels.controller';
 import { LevelsService } from './levels.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Level])],
+    imports: [TypeOrmModule.forFeature([Level, User])],
     providers: [LevelsService],
     controllers: [LevelsController]
 })
