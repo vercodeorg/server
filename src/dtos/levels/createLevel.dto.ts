eaf3bd63c674a8d895be31b5aeb1c77ee2340a61
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-08-01 20:11:17 -0300

    feat: update leveldto

--- src/dtos/levels/createLevel.dto.ts
+++ src/dtos/levels/createLevel.dto.ts
@@ -5,3 +5,22 @@ export class CreateLevelDTO {
     status: Status
     unlockCost: number
 }
+
+export const CreateLevel = 
+[
+    {
+        name: "00 - Fundamentals and Instructions",
+        stauts: Status.BLOCKED,
+        unlockCost: 0
+    },
+    {
+        name: "01 - First steps in C",
+        stauts: Status.BLOCKED,
+        unlockCost: 300
+    },
+    {
+        name: "02 - Improving coding skills",
+        stauts: Status.BLOCKED,
+        unlockCost: 750
+    }
+]
