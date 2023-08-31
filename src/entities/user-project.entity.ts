8a3d5cf9c810bf768be44f3f19e1d1fb6c60c293
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-08-31 20:00:48 -0300

    create many to many user_project relation

---
+++ src/entities/user-project.entity.ts
@@ -0,0 +1,23 @@
+import { Status } from "src/types/status";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./user.entity";
+import { Project } from "./project.entity";
+
+@Entity("users_projects")
+export class UserProject {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({name: "project_status", type: "enum", enum: Status, default: Status.BLOCKED})
+    projectStatus: Status
+
+    @Column({name: "if_unlocked_with_coins" ,type: "boolean", default: false})
+    ifUnlockedWithCoins: boolean
+
+    @ManyToOne(() => User, user => user.usersProjects)
+    user: User
+
+    @ManyToOne(() => Project, project => project.usersProjects)
+    project: Project
+}
