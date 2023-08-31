d8f32bb41e2542c2206d9402cdcdfc51e8d6e933
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-08-31 20:02:18 -0300

    create users service and module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,19 +3,36 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDTO } from 'src/dtos/users/createUser.dto';
 import { UpdateUserDTO } from 'src/dtos/users/updateUser.dto';
 import { User } from 'src/entities/user.entity';
-import { Repository } from 'typeorm';
+import { UsersLevelsService } from 'src/users-levels/users-levels.service';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
 
     constructor(
         @InjectRepository(User)
-        private userRepository: Repository<User>
+        private userRepository: Repository<User>,
+        private usersLevelsService: UsersLevelsService,
+        private dataSource: DataSource 
     ){}
 
-    create(createUserDTO: CreateUserDTO){
-        const newUser = this.userRepository.create({...createUserDTO})
-        return this.userRepository.save(newUser)
+    async createNewUserAndConnectExercises(createUserDTO: CreateUserDTO){
+        const queryRunner = this.dataSource.createQueryRunner();
+
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            const newUser = this.userRepository.create(createUserDTO);
+            await this.userRepository.save(newUser)
+            await this.usersLevelsService.create(newUser)
+        }
+        catch(err){
+            console.log(err)
+            await queryRunner.rollbackTransaction();
+        }
+        finally{
+            await queryRunner.release();
+        }
     }
 
     delete(id: number){
