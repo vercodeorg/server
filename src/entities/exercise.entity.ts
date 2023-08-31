e5359551031047d447f9e93cb3044fbb04ed5628
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-08-31 19:58:55 -0300

    create exercise entity

--- src/entities/exercise.entity.ts
+++ src/entities/exercise.entity.ts
@@ -1,6 +1,7 @@
 import { Technology } from "src/types/technology";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Project } from "./project.entity";
+import { UserExercise } from "./user-exercise.entity";
 
 @Entity("exercises")
 export class Exercise {
@@ -17,15 +18,27 @@ export class Exercise {
     @Column({type: "int", name: "xp_to_win"})
     xpToWin: number
     
-    @Column({type: "enum", enum: Technology, array: true})
-    technologies: Technology[]
+    @Column("simple-array")
+    technologies: string[]
 
-    @Column()
-    instructions: string
+    @Column({name: "turn_in_directory"})
+    turnInDirectory: string
+
+    @Column("simple-array", {name: "files_to_turn_in"})
+    filesToTurnIn: string[]
+
+    @Column("simple-array", {name: "allowed_functions"})
+    allowedFunctions: string[]
+
+    @Column({name: "image_instructions"})
+    imageInstructions: string
     
-    @Column({name: "code_result"})
+    @Column({name: "code_result", nullable: true})
     codeResult: string
 
     @ManyToOne(() => Project, project => project.exercises)
     project: Project
+
+    @OneToMany(() => UserExercise, usersExercises => usersExercises.exercise)
+    usersExercise: UserExercise[]
 }
