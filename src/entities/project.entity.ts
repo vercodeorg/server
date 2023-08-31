d2c495eb4eb2a7fcb34820ea2d7d157e9db980cb
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-08-31 19:59:24 -0300

    create project entity

--- src/entities/project.entity.ts
+++ src/entities/project.entity.ts
@@ -1,8 +1,8 @@
 import { Difficulty } from "src/types/difficulty";
-import { Status } from "src/types/status";
 import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Exercise } from "./exercise.entity";
 import { Level } from "./level.entity";
+import { UserProject } from "./user-project.entity";
 
 @Entity("projects")
 export class Project {
@@ -12,9 +12,6 @@ export class Project {
 
     @Column()
     name: string
-
-    @Column({type: "enum", enum: Status, default: Status.BLOCKED})
-    status: Status
     
     @Column({type: "enum", enum: Difficulty})
     difficulty: Difficulty
@@ -27,5 +24,7 @@ export class Project {
 
     @OneToMany(() => Exercise, exercise => exercise.project)
     exercises: Exercise[]
- 
+
+    @OneToMany(() => UserProject, usersProject => usersProject.user)
+    usersProjects: UserProject
 }
