607870a50db9e75911dc364cf8576f72bdb13337
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-09-01 17:29:13 -0300

    basic crud for exercise controller

--- src/exercises/exercises.controller.ts
+++ src/exercises/exercises.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
+import { CreateExerciseDTO } from 'src/dtos/exercises/createExercise.dto';
+import { UpdateExerciseDTO } from 'src/dtos/exercises/updateExercise.dto';
+import { ExercisesService } from './exercises.service';
 
 @Controller('exercises')
-export class ExercisesController {}
+export class ExercisesController {
+
+    constructor(
+        private exercisesService: ExercisesService
+    ){}
+
+    @Post()
+    createExercise(@Body() createExerciseDTO: CreateExerciseDTO){
+        this.exercisesService.create(createExerciseDTO)
+    }
+
+    @Delete(':id')
+    deleteExercise(@Param('id') id: number){
+        this.exercisesService.delete(id);
+    }
+
+    @Put(':id')
+    updateExercise(@Param('id') id: number, @Body() updateExerciseDTO: UpdateExerciseDTO){
+        this.exercisesService.update(id, updateExerciseDTO);
+    }
+
+}
