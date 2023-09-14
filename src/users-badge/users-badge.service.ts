0331fb4c78c8985f746010f7ba728c3bc40168d6
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-09-14 00:12:08 -0300

    feat: create users-badge module and service

---
+++ src/users-badge/users-badge.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BadgesService } from 'src/badges/badges.service';
+import { UserBadges } from 'src/entities/user-badges.entity';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersBadgeService {
+
+    constructor(
+        @InjectRepository(UserBadges)
+        private usersBadgeRepository: Repository<UserBadges>,
+        private badgesService: BadgesService
+    ){}
+
+    async connectBadgeToUser(user: User){
+        const newUserBadge = this.usersBadgeRepository.create();
+        newUserBadge.user = user;
+        newUserBadge.badge = await this.badgesService.findOne(1);
+        return await this.usersBadgeRepository.save(newUserBadge);
+    }
+
+
+}
