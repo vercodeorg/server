478a0ca388e6614becf690c9cf18c5e6425ff65a
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-09-18 01:21:36 -0300

    feat: change userPoints from users_points to user entity

--- src/entities/user-points.entity.ts
+++ src/entities/user-points.entity.ts
@@ -1,19 +1,16 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
 import { RankProgress } from "./rank-progress.entity";
 
 @Entity('users_points')
-export class UserPoints{
+export class UserPoints {
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @Column({name: 'user_current_points', default: 0, type: "int"})
-    userCurrentPoints: number
+  @ManyToOne(() => User, user => user.usersPoints)
+  user: User
 
-    @ManyToOne(() => User, user => user.usersPoints)
-    user: User
-
-    @ManyToOne(() => RankProgress, rankProgress => rankProgress.usersPoints)
-    rankProgress: RankProgress
+  @ManyToOne(() => RankProgress, rankProgress => rankProgress.usersPoints)
+  rankProgress: RankProgress
 }
