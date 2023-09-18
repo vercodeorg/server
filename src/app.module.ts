05d266121b40bd84cedac38cc9ee862f2af64e15
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-09-18 00:13:16 -0300

    feat: upload entities on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,14 @@ import { LevelsModule } from './levels/levels.module';
 import { ProjectsModule } from './projects/projects.module';
 import { RankProgressModule } from './rank-progress/rank-progress.module';
 import { TechProgressModule } from './tech-progress/tech-progress.module';
+import { UsersBadgeModule } from './users-badge/users-badge.module';
 import { UsersEventsModule } from './users-events/users-events.module';
 import { UsersExercisesModule } from './users-exercises/users-exercises.module';
 import { UsersLevelsModule } from './users-levels/users-levels.module';
 import { UsersPointsModule } from './users-points/users-points.module';
 import { UsersProjectsModule } from './users-projects/users-projects.module';
+import { UsersTechProgressModule } from './users-tech-progress/users-tech-progress.module';
 import { UsersModule } from './users/users.module';
-import { UsersBadgeModule } from './users-badge/users-badge.module';
 
 @Module({
   imports: [
@@ -43,6 +44,7 @@ import { UsersBadgeModule } from './users-badge/users-badge.module';
     RankProgressModule,
     UsersPointsModule,
     UsersBadgeModule,
+    UsersTechProgressModule,
   ],
   controllers: [AppController],
   providers: [AppService],
