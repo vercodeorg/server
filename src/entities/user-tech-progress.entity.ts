0853f1ad67fc2b896826cf1c0f6740a50d46bccf
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-09-18 01:23:05 -0300

    feat: created user-tech-progress entity

---
+++ src/entities/user-tech-progress.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { TechProgress } from "./tech-progress.entity";
+import { User } from "./user.entity";
+
+@Entity("users_tech_progress")
+export class UserTechProgress {
+
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({type: "int", name: "xp_points"})
+  xpPoints: number
+
+  @ManyToOne(() => User, user => user.usersTechProgress)
+  user: User
+
+  @ManyToOne(() => TechProgress, techProgress => techProgress.usersTechProgress)
+  techProgress: TechProgress
+}
