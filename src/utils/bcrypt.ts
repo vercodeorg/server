d288949df9a61a1cd9b9a11c64c9e3283fa70404
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-11-07 23:25:52 -0300

    feat: bcrypt to hash passwords

---
+++ src/utils/bcrypt.ts
@@ -0,0 +1,11 @@
+import * as bcrypt from 'bcrypt'
+
+
+export async function encodePassword(rawPassword:string){
+    const saltOrRounds = 10;
+    return bcrypt.hash(rawPassword, saltOrRounds);
+}
+
+export async function comparePassword(rawPassword: string, hashPassword: string){
+    return bcrypt.compare(rawPassword, hashPassword);
+}
