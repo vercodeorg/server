778566645c109a1aa6dbde57975e76216e0f1aaf
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-11-07 23:26:29 -0300

    feat: enableCors and add api global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import * as dotenv from "dotenv"
 async function bootstrap() {
   dotenv.config()
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
